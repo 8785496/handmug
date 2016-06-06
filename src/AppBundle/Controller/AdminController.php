dc87fd4e137eab822139db1efd6a35a61bfe2640
8785496 <georg.88@mail.ru>
2016-06-06 16:23:56 +0600

    edit admin

--- src/AppBundle/Controller/AdminController.php
+++ src/AppBundle/Controller/AdminController.php
@@ -5,6 +5,7 @@ namespace AppBundle\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
 
 class AdminController extends Controller
 {
@@ -13,7 +14,65 @@ class AdminController extends Controller
      */
     public function indexAction(Request $request)
     {
-        // replace this example code with whatever you need
-        return $this->render('admin/index.html.twig', []);
+        $repository = $this->getDoctrine()->getRepository('AppBundle:Visitor');
+        $query = $repository->createQueryBuilder('v')
+            ->where('v.time >= :date')
+            ->setParameter('date', new \DateTime('today'))
+            ->orderBy('v.id', 'DESC')
+            ->getQuery();
+
+        $visitors = $query->getResult();
+
+        return $this->render('admin/index.html.twig', [
+            'caption' => 'Посетители сегодня',
+            'visitors' => $visitors
+        ]);
+    }
+
+    /**
+     * @Route("/admin/yesterday", name="yesterday")
+     */
+    public function yesterdayAction(Request $request)
+    {
+        $repository = $this->getDoctrine()->getRepository('AppBundle:Visitor');
+        $query = $repository->createQueryBuilder('v')
+            ->where('v.time >= :yesterday AND v.time < :today')
+            ->setParameter('today', new \DateTime('today'))
+            ->setParameter('yesterday', new \DateTime('yesterday'))
+            ->orderBy('v.id', 'DESC')
+            ->getQuery();
+
+        $visitors = $query->getResult();
+
+        return $this->render('admin/index.html.twig', [
+            'caption' => 'Посетители вчера',
+            'visitors' => $visitors
+        ]);
+    }
+
+    /**
+     * @Route("/admin/order", name="adminOrder")
+     */
+    public function orderAction(Request $request)
+    {
+        return new Response('Страница в разработке.');
+    }
+
+    /**
+     * @Route("/admin/mail", name="adminMail")
+     */
+    public function mailAction(Request $request)
+    {
+        $repository = $this->getDoctrine()->getRepository('AppBundle:Email');
+        $query = $repository->createQueryBuilder('m')
+            ->orderBy('m.id', 'DESC')
+            ->getQuery();
+
+        $mails = $query->getResult();
+
+        return $this->render('admin/mail.html.twig', [
+            'caption' => 'Письма с сайта',
+            'mails' => $mails
+        ]);
     }
 }
